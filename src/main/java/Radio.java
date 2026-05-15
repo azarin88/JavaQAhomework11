dae65a58af98a163fe6b1de5ab606244d403a72b
Azarin Maxim <azarin.m88@gmail.com>
2026-05-15 16:19:29 +0300

    Initial commit

---
+++ src/main/java/Radio.java
@@ -0,0 +1,36 @@
+public class Radio {
+    private int numberOfTheCurrentRadioStation = 0;
+    private int volumeOfTheSound = 0;
+
+    public void next() {
+        numberOfTheCurrentRadioStation = numberOfTheCurrentRadioStation < 9 ? ++numberOfTheCurrentRadioStation : 0;
+    }
+
+    public void prev() {
+        numberOfTheCurrentRadioStation = numberOfTheCurrentRadioStation > 0 ? --numberOfTheCurrentRadioStation : 9;
+    }
+
+    public void setNumberOfRadioStation(int numberOfRadioStation) {
+        numberOfTheCurrentRadioStation = numberOfRadioStation >= 0 && numberOfRadioStation < 10 ? numberOfRadioStation : numberOfTheCurrentRadioStation;
+    }
+
+    public int getNumberOfRadioStation() {
+        return numberOfTheCurrentRadioStation;
+    }
+
+    public void volumeUp() {
+        volumeOfTheSound = volumeOfTheSound < 100 ? ++volumeOfTheSound : 100;
+    }
+
+    public void volumeDown() {
+        volumeOfTheSound = volumeOfTheSound > 0 ? --volumeOfTheSound : 0;
+    }
+
+    public void setVolumeOfTheSound(int volume) {
+        volumeOfTheSound = volume >= 0 && volume < 101 ? volume : volumeOfTheSound;
+    }
+
+    public int getVolumeOfTheSound() {
+        return volumeOfTheSound;
+    }
+}
---
+++ src/test/java/RadioTest.java
@@ -0,0 +1,57 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class RadioTest {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/DataTestsForSetNumberOfRadioStation")
+    void TestSetNumberOfRadioStation(int radioStation, int expected) {
+        Radio radio = new Radio();
+        radio.setNumberOfRadioStation(radioStation);
+        Assertions.assertEquals(radio.getNumberOfRadioStation(), expected);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/DataTestsForNext")
+    void positiveTestNext(int radioStation, int expected) {
+        Radio radio = new Radio();
+        radio.setNumberOfRadioStation(radioStation);
+        radio.next();
+        Assertions.assertEquals(radio.getNumberOfRadioStation(), expected);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/DataTestsForPrev")
+    void positiveTestPrev(int radioStation, int expected) {
+        Radio radio = new Radio();
+        radio.setNumberOfRadioStation(radioStation);
+        radio.prev();
+        Assertions.assertEquals(radio.getNumberOfRadioStation(), expected);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/DataTestsForSetVolumeOfTheSound")
+    void TestSetVolumeOfTheSound(int volume, int expected) {
+        Radio radio = new Radio();
+        radio.setVolumeOfTheSound(volume);
+        Assertions.assertEquals(radio.getVolumeOfTheSound(), expected);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/DataTestsForVolumeUp")
+    void positiveTestVolumeUp(int volume, int expected) {
+        Radio radio = new Radio();
+        radio.setVolumeOfTheSound(volume);
+        radio.volumeUp();
+        Assertions.assertEquals(radio.getVolumeOfTheSound(), expected);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/DataTestsForVolumeDown")
+    void positiveTestVolumeDown(int volume, int expected) {
+        Radio radio = new Radio();
+        radio.setVolumeOfTheSound(volume);
+        radio.volumeDown();
+        Assertions.assertEquals(radio.getVolumeOfTheSound(), expected);
+    }
+}
